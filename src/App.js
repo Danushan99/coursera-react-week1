7d367bdddb94efc5cf27fc58e235edce4978fa19
Danushan <it20051648@my.sliit.lk>
2021-11-28 13:14:38 +0530

    implement dish.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import Menu from './components/MenuComponens';
 import { DISHES} from './shared/dishes';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state= {
+      dishes : DISHES
+    };
+  }
   render(){
   return (
     <div className="App">
